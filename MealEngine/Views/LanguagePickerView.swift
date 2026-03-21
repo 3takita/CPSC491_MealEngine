24b484aa1d6e485b8e75b7612cc8ab072d0d7cca
cyxxxsnd <1658328404@qq.com>
2026-03-20 23:44:17 -0700

    Remove LanguagePickerView implementation

--- MealEngine/Views/LanguagePickerView.swift
+++ MealEngine/Views/LanguagePickerView.swift
@@ -1,17 +1,1 @@
-import SwiftUI
 
-struct LanguagePickerView: View {
-    @EnvironmentObject var languageManager: LanguageManager
-
-    var body: some View {
-        Picker(languageManager.text(for: "switch_language"), selection: $languageManager.currentLanguage) {
-            Text("English").tag("en")
-            Text("中文").tag("zh")
-        }
-        .pickerStyle(.segmented)
-        .onChange(of: languageManager.currentLanguage) { _, newValue in
-            languageManager.setLanguage(newValue)
-        }
-        .padding(.horizontal)
-    }
-}
